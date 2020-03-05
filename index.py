9b110ae7bbd7342ed1ed4a368aba2fb0df4fcb6f
Marcos Martinez <mamrdev@gmail.com>
2020-03-05 19:27:29 -0400

    Update index.py
    updating final code

--- index.py
+++ index.py
@@ -1,21 +1,85 @@
-from io import *
+import os
+import os.path
 
-#opciones
-main_options=int(input(' 1. para crear nuevo objeto \n 2. para editar un objeto \n 3. para borra un objeto \n 4. para leer objetos guardados \n Introdusca un Numero:'))
 
-#Funcion Guardar Objetos
-def save_objet(self,inname,inprice):
-    archive_text=open("objetos_inventario.txt", "r+")
-    lista_texto=inname,inprice
+#Comprobacion archivo de texto, en dado caso que no exista o este fuera borrado se creara uno nuevo
+if os.path.isfile("objetos.txt"):
+    pass
+else:
+    f = open ('objetos.txt','w')
+    f.write('Producto , Unidades de Producto, Valor de cada producto\n')
+    f.close()
+    
+#funciones basicas del programa
+def agregarproducto(nombre,cantidad,valorunit):
+    f = open ('objetos.txt','a')
+    f.write('\n' + nombre)
+    f.write(',' + cantidad)
+    f.write(',' + valorunit)
+    f.close()
 
-    archivo_texto.seek(0)
+def leerobjetos():
+    f = open ('objetos.txt','r')
+    mensaje = f.read()
+    print(mensaje)
+    f.close()
+    
+def formateararchivo():    
+    f = open ('objetos.txt','w')
+    f.seek(0)
+    f.truncate()
+    f.write('Productos,Cantidad de Productos,Valor de cada producto\n')
+    f.close()
 
-    archivo_texto.writelines(objetos_inventario)
+#Inicio del programa
+print("Bienvenido al sistema de almacen\n")
+bucle=0
 
-    archivo_texto.close()
+while bucle==0:
 
-if main_options==1:
+    opciones=int(input('Coloque 1 para agregar nuevo producto \nColoque 2 para leer productos guardados \nColoque 3 para borrar todo el contenido del archivo \nColoque 4 para salir del programa \n Que desea hacer?:'))
+    
+    if opciones==1:
+        nombre=input('Nombre del producto: ')
+        cantidad=input('Introdusca la cantidad de productos de entrada: ')
+        valorunit=input('Introdusca el valor por unidad del producto: ')        
+        agregarproducto(nombre,cantidad,valorunit)
+        loop2=0
+        while loop2==0:
+            decision=int(input('Desea agregar otro producto?\n Coloque 1 para si \n Coloque 2 para no \n Que desea hacer: '))
+            
+            if decision==1:
+                nombre=input('Nombre del producto: ')
+                cantidad=input('Introdusca la cantidad de productos de entrada: ')
+                valorunit=input('Introdusca el valor por unidad del producto: ')        
+                agregarproducto(nombre,cantidad,valorunit)
 
-    inname=input('Nombre del objeto: ')
-    inprice=input('Precio: ')
-    save_objet(inname,inprice)
+            elif decision==2:
+                break
+
+            else:
+                decision=int(input('Opcion invalida intente de nuevo: '))
+
+    elif opciones==2:
+        leerobjetos()
+        limpiarconsola=int(input('Desea continuar limpiando la consola? \n Coloque 1 para si \n Coloque 2 para no\n Que desea hacer: '))
+        
+        if limpiarconsola==1:
+            clear = lambda: os.system('cls')
+            clear()
+
+        elif limpiarconsola==2:
+            pass
+    
+        else:
+            limpiarconsola=int(input('Opcion invalida intente de nuevo: '))
+
+    elif opciones==3:
+        formateararchivo()
+
+    elif opciones==4:
+        break
+
+    else:
+        print('\n Error, seleccione una opcion valida: \n')
+        
